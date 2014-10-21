1180c41eb8ab41f6b527b13ddae54cc782422f62
iguto <clc.iguto@gmail.com>
2014-10-22 01:13:26 +0900

    GemfileにRSpecを追加 / guard-rspecを使う初期手順

--- Gemfile
+++ Gemfile
@@ -1,4 +1,10 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+group :development do
+  gem 'pre-commit'
+  gem 'guard-rspec', require: false
+  gem 'rspec'
+end
+
 gem 'eventmachine'
---
+++ spec/position_spec.rb
@@ -0,0 +1,12 @@
+p pwd: Dir.pwd
+# require '/position'
+# require './spec/spec_helper'
+require 'spec_helper'
+require 'position'
+
+describe Position do
+  describe "" do
+    it "" do
+    end
+  end
+end
