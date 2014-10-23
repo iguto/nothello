a876f496fb927620c8ae4480ce9846655821e07c
iguto <clc.iguto@gmail.com>
2014-10-24 04:45:34 +0900

    斜め方向のイテレーションテストコードを追加

--- spec/board_iterator_spec.rb
+++ spec/board_iterator_spec.rb
@@ -61,4 +61,66 @@ describe PanelIterator do
       it { expect(panel_iterator.left).to be_kind_of Panel }
     end
   end
+
+
+  describe "#right_up" do
+    before { panel_iterator.set_pos pos }
+    context "盤面内の移動の場合" do
+      let(:pos) { Position.new(1, 1) }
+      it { expect(panel_iterator.right_up).to be_kind_of Panel }
+    end
+    context "盤面外へ移動してしまう場合(上方向)" do
+      let(:pos) { Position.new(2, 0) }
+      it { expect(panel_iterator.right_up).to be nil }
+    end
+    context "盤面外へ移動してしまう場合(右方向)" do
+      let(:pos) { Position.new(Board::SIZE-1, 2) }
+      it { expect(panel_iterator.right_up).to be nil }
+    end
+  end
+  describe "#left_up" do
+    before { panel_iterator.set_pos pos }
+    context "盤面内の移動の場合" do
+      let(:pos) { Position.new(1, 1) }
+      it { expect(panel_iterator.left_up).to be_kind_of Panel }
+    end
+    context "盤面外へ移動してしまう場合(上方向)" do
+      let(:pos) { Position.new(2, 0) }
+      it { expect(panel_iterator.left_up).to be nil }
+    end
+    context "盤面外へ移動してしまう場合(左方向)" do
+      let(:pos) { Position.new(0, 2) }
+      it { expect(panel_iterator.left_up).to be nil }
+    end
+  end
+  describe "#right_down" do
+    before { panel_iterator.set_pos pos }
+    context "盤面内の移動の場合" do
+      let(:pos) { Position.new(1, 1) }
+      it { expect(panel_iterator.right_down).to be_kind_of Panel }
+    end
+    context "盤面外へ移動してしまう場合(下方向)" do
+      let(:pos) { Position.new(2, Board::SIZE-1) }
+      it { expect(panel_iterator.right_down).to be nil }
+    end
+    context "盤面外へ移動してしまう場合(右方向)" do
+      let(:pos) { Position.new(Board::SIZE-1, 2) }
+      it { expect(panel_iterator.right_down).to be nil }
+    end
+  end
+  describe "#left_down" do
+    before { panel_iterator.set_pos pos }
+    context "盤面内の移動の場合" do
+      let(:pos) { Position.new(1, 1) }
+      it { expect(panel_iterator.left_down).to be_kind_of Panel }
+    end
+    context "盤面外へ移動してしまう場合(下方向)" do
+      let(:pos) { Position.new(2, Board::SIZE-1) }
+      it { expect(panel_iterator.left_down).to be nil }
+    end
+    context "盤面外へ移動してしまう場合(左方向)" do
+      let(:pos) { Position.new(0, 2) }
+      it { expect(panel_iterator.left_down).to be nil }
+    end
+  end
 end
